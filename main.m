e090962646b69ab2b706365b3ecdd4f71dad242b
Luis Alejandro <lpascuazac@unal.edu.co>
2022-11-06 22:16:59 -0500

    Last commit before first advance presentation

--- main.m
+++ main.m
@@ -6,12 +6,14 @@ tStart = cputime;
 clc;
 
 
-load("MoMbenchmark\MoMbenchmarkData2.mat");
+load("MoMbenchmark\MoMbenchmarkData1.mat");
 
 
 A = triverts(1,:)';
 B = triverts(2,:)';
 C = triverts(3,:)';
+
+v = [A, B, C]
 
 Icalc = zeros(length(r),1);
 
@@ -19,27 +21,45 @@ for ii=1:length(r)
    Icalc(ii, :) = triang_int(r(ii,:)', A, B, C); 
 end
 
-x_axis = r(:,1);
+x_axis = r(:,3);
+
+figure()
+hold on;
+patch(v(1,:), v(2,:), v(3,:), 'FaceColor', [0, 0, 0], 'linestyle', ':')
+% plot3(A(1), A(2), A(3), 'ok', 'MarkerFaceColor', 'y')
+% plot3(B(1), B(2), B(3), 'ok', 'MarkerFaceColor', 'b')
+% plot3(C(1), C(2), C(3), 'ok', 'MarkerFaceColor', 'r')
+%plot3(r(:, 1), r(:, 2), r(:, 3), '-r', 'MarkerFaceColor', 'r')
+grid on, grid minor;
+axis image;
+view([-37 -43]);
+camroll(-360);
+%axis([[-5 5 -0.05 0.15 -1 1]])
+xlabel('x-axis')
+ylabel('y-axis')
+zlabel('z-axis')
 
 figure()
 subplot(2, 1, 1)
 hold on
-plot(x_axis, Ival, '--')
-plot(x_axis, Icalc, ':')
+plot(x_axis, Ival, '--b', 'LineWidth', 1.2)
+plot(x_axis, Icalc, ':r', 'LineWidth', 1.5)
 legend("Benchmark", "Calculation")
 %grid on, grid minor;
-
-M = Ival-Icalc;
+xlabel("x position [m]")
+ylabel("Integral [dS/R]")
+M = 100*(Ival-Icalc)./Ival;
 [~, ind] = max(abs(M), [], 1, 'linear');
-
 
 subplot(2, 1, 2)
 hold on
-plot(x_axis, (M))
+plot(x_axis, (M), 'b')
 yline(M(ind), '--r', string(M(ind)),...
       'LabelHorizontalAlignment','center',...
       'LabelVerticalAlignment','middle');
-legend
+legend("Error", "Max. error")
+xlabel("x position [m]")
+ylabel("Porcentual error [%]")
 %grid on, grid minor;
 
 %whos
--- test_v2.m
+++ test_v2.m
@@ -2,24 +2,45 @@
 
 clc;
 
-obj = readObj('Test_shapes\Surf_3.obj');
+obj = readObj('Test_shapes\Surf_5.obj');
 
 V = obj.v(:, 1:3)';
 F = [obj.f.v]';
 
+epsilon_0 = 8.8541878128E-12;
+ke = 1/(4*pi*epsilon_0);
+
 r = [0, 0, 1000]';
 
-R = [zeros(100, 1), zeros(100, 1), linspace(10, 100, 100)']';
-
-figure();
-hold on;
+R = [zeros(100, 1), zeros(100, 1), linspace(5, 100, 100)']';
 
 FC = [1 1 1];
+
+figure();
+subplot(1, 2, 1)
+hold on;
+patch('Faces',F','Vertices',V', 'FaceColor', FC)
+view([45 35.264]);
+axis image
+grid on;
+grid minor;
+xlabel('x-axis')
+ylabel('y-axis')
+zlabel('z-axis')
+title(string(size(F,2)) + " triangles figure")
+
+subplot(1, 2, 2)
+hold on;
+
 patch('Faces',F','Vertices',V', 'FaceColor', FC)
 %plot3([0, r(1)], [0, r(2)], [0, r(3)], 'or', 'MarkerFaceColor', 'r')
-plot3(r(1), r(2), r(3), 'or', 'MarkerFaceColor', 'r')
-axis vis3d
-axis padded
+%plot3(r(1), r(2), r(3), 'or', 'MarkerFaceColor', 'r')
+plot3([R(1,1), R(1,end)], [R(2,1), R(2,end)], [R(3,1), R(3,end)], '-or', 'MarkerFaceColor', 'r')
+%axis vis3d
+%axis padded
+view([45 35.264]);
+%camroll(-360);
+axis image
 grid on;
 grid minor;
 xlabel('x-axis')
@@ -37,6 +58,26 @@ for jj = 1:size(R,2)
         Icalc(jj, :) = Icalc(jj, :) + triang_int(R(:, jj), A(:,ii), B(:,ii), C(:,ii));
     end
 end
+
+Q = 1/size(F,2);
+
+figure()
+hold on
+plot(R(3, :)', Q*Icalc, '.:b', 'LineWidth', 1.5)
+plot(R(3, :)', 1./R(3, :)', '--r', 'LineWidth', 1.5)
+legend("Calc.", "1/r ref.")
+axis padded
+grid on;
+grid minor;
+xlabel("r distance [m]")
+ylabel("Integral [(dS/R)*(1/#Triangles)]")
+
+%%
+figure()
+hold on
+plot(R(3, :)', 1*Icalc*Q)
+plot(R(3, :)', 1*1./R(3, :)')
+
 
 figure()
 hold on
